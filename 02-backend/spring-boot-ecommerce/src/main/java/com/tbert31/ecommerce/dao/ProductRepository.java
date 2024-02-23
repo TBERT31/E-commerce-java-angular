ba54f8252e869178e192d4c9f34cc0686b1343da
tbert31 <berteau.thomas@hotmail.fr>
2024-02-23 08:24:08 +0100

    spring boot rest api

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/tbert31/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.tbert31.ecommerce.dao;
+
+import com.tbert31.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
